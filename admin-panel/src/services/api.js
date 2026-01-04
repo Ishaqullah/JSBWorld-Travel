9732745406a0fd96c42971a20f14f66cd8c910ec
Ishaqullah <ishaq.siddiqui1401@gmail.com>
2026-01-04 21:06:29 +0500

    fix: admin panel api route change

--- admin-panel/src/services/api.js
+++ admin-panel/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://crucial-charmane-jsbworld786-acf8c548.koyeb.app/api';
 
 const api = axios.create({
   baseURL: API_URL,
